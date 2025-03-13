87bf8d79f5db108c6bfee33bbf823785d0ccda6a
VictorDMe <victormenezesna@gmail.com>
2025-03-13 20:34:22 -0300

    refactor: refatorado teste desktop

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,25 +3,11 @@ describe('Voucher', () => {
     cy.setDesktopViewport();
     cy.login();
     cy.clickVouchers();
-    cy.get('.select').click(); // Clica em selecionar estado
-    cy.wait(2000);
-    cy.get('.action-sheet-button-inner').contains('Distrito Federal').click({ force: true }); // Selecina estado
-    cy.get('.content-container button').click(); // Clica em continuar
-    cy.get('.item.ng-star-inserted img.regular[title="Campanha para testes alterada [Não apague]"]').first().click(); // Clica no primeiro elemento correspondente
-    cy.get(':nth-child(1) > advertisement-card > .container').click(); //Clica no voucher
-    cy.get(':nth-child(3) > .voucher-counter > .voucher-plus-btn > img').click(); //Clica em +
-    cy.get('.payment-display-container > .action-container > .md').click(); //Clica em adicionar forma de pagamento
-    cy.get('.online-payment > :nth-child(2)').contains('Pix').click(); //Clica em Pix
-    cy.get('body').then(($body) => {
-      if ($body.find(':nth-child(4) > .action-container > .ng-star-inserted').length) {
-        cy.get(':nth-child(4) > .action-container > .ng-star-inserted').click(); // clicar em adicionar cpf
-        cy.get('#cpf').type(Cypress.env('cpf'));
-        cy.get('#phone').type(Cypress.env('phone'));
-        cy.get('.container > button').click(); // clicar em salvar
-      }
-    });
-    cy.get('bag-button.ng-star-inserted > .container').click(); 
-    cy.get('.is-primary').click(); // clicar em finalizar compra
+    cy.selecionarEstado('Distrito Federal');
+    cy.selecionarVoucher('Campanha para testes alterada [Não apague]');
+    cy.adicionarFormaPagamento('Pix');
+    cy.preencherDados();
+    cy.finalizarCompra();
   });
 
   // Comentando o teste mobile
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,6 +44,45 @@ Cypress.Commands.add('clickVouchers', () => {
   cy.get('.nav-bar-content > :nth-child(3)').click(); 
 });
 
+Cypress.Commands.add('finalizePurchase', () => {
+  cy.get('bag-button.ng-star-inserted > .container').click();
+  cy.get('.is-primary').click(); 
+});
+
+Cypress.Commands.add('selecionarEstado', (estado) => {
+  cy.get('.select').click(); 
+  cy.wait(2000);
+  cy.get('.action-sheet-button-inner').contains(estado).click({ force: true }); 
+  cy.get('.content-container button').click(); 
+});
+
+Cypress.Commands.add('selecionarVoucher', (tituloVoucher) => {
+  cy.get(`.item.ng-star-inserted img.regular[title="${tituloVoucher}"]`).first().click(); 
+  cy.get(':nth-child(1) > advertisement-card > .container').click(); 
+  cy.get(':nth-child(3) > .voucher-counter > .voucher-plus-btn > img').click(); 
+});
+
+Cypress.Commands.add('adicionarFormaPagamento', (metodo) => {
+  cy.get('.payment-display-container > .action-container > .md').click(); 
+  cy.get('.online-payment').contains(metodo).click(); 
+});
+
+Cypress.Commands.add('preencherDados', () => {
+  cy.get('body').then(($body) => {
+    if ($body.find(':nth-child(4) > .action-container > .ng-star-inserted').length) {
+      cy.get(':nth-child(4) > .action-container > .ng-star-inserted').click(); 
+      cy.get('#cpf').type(Cypress.env('cpf'));
+      cy.get('#phone').type(Cypress.env('phone'));
+      cy.get('.container > button').click(); 
+    }
+  });
+});
+
+Cypress.Commands.add('finalizarCompra', () => {
+  cy.get('bag-button.ng-star-inserted > .container').click();
+  cy.get('.is-primary').click(); 
+});
+
 //
 //
 // -- This is a child command --
